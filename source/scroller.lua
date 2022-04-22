3b433c28387b4dbbeb0fdbd4ede8ebb775e9857d
Spencatro <spencatro.public@gmail.com>
2022-04-21 19:35:47 -0700

    Automatically progress text instead of manually adding lines with button presses

--- source/scroller.lua
+++ source/scroller.lua
@@ -5,7 +5,10 @@ local gfx <const> = playdate.graphics
 
 local LINE_MIN <const> = -85
 local t_lineCurrent = LINE_MIN
+local t_lineMax = t_lineCurrent
+local t_lineThreshold = t_lineCurrent
 local g_textLineTable = {}
+local g_preparedTextLineTable = {}
 local g_blurbCount = 0
 
 local standardFont
@@ -17,7 +20,7 @@ cursorSprite:addState("left", 1, 9, {tickStep = 2, yoyo = true}, true)
 cursorSprite:moveTo(20,20)
 
 -- SET THIS TO YOUR FONT'S HEIGHT
-local fontHeight = 20
+local fontHeight = 0
 
 local function fprint(line)
     frameDebug(line, "[Scroller]")
@@ -31,12 +34,25 @@ local function splitLine(line)
     return words
 end
 
-function scrollerInit()
+function scrollerInit(lines)
     standardFont = gfx.font.new('fonts/font-full-circle')
     halvedFont = gfx.font.new('fonts/font-full-circle-halved')
+    gfx.setFont(standardFont)
+    _, fontHeight = gfx.getTextSize("aA!09gp")
+    fontHeight += 5
+
+    local initialLine = table.remove(lines, 1)
+    addTextLine(initialLine)
+    for idx, line in ipairs(lines) do
+        prepareLine(line)
+    end
+
+    t_lineThreshold += nextLineHeight()
 end
 
 function addTextLine(line, dontBreak)
+    print(line)
+    print(calcTotalHeight(line))
     dontBreak = dontBreak or false
     g_blurbCount += 1
     local words = splitLine(line)
@@ -48,21 +64,70 @@ function addTextLine(line, dontBreak)
             local currentLineTemp = currentLine .. nextWord .. " "
             local w, h = gfx.getTextSize(currentLineTemp)
             if w < PLAYDATE_MAX_WIDTH - 60 then
-            currentLine = currentLineTemp
-            currentLineWidth = w
+                currentLine = currentLineTemp
+                currentLineWidth = w
             else
-            table.insert(words, 1, nextWord)
-            break
+                table.insert(words, 1, nextWord)
+                break
             end
             nextWord = table.remove(words, 1)
             if nextWord == nil then
-            break
+                break
             end
         end
         table.insert(g_textLineTable, currentLine)
     end
     table.insert(g_textLineTable, "<br>")
- end
+end
+
+function prepareLine(line)
+    table.insert(g_preparedTextLineTable, line)
+end
+
+function addNextPreparedLine()
+    local nextLine = table.remove(g_preparedTextLineTable, 1)
+    if nextLine then
+        addTextLine(nextLine)
+    end
+end
+
+function calcTotalHeight(line)
+    local totalHeight = fontHeight
+    local words = splitLine(line)
+    local nextWord = table.remove(words, 1)
+    while nextWord ~= nil do
+        local currentLineWidth = 0
+        local currentLine = ""
+        while currentLineWidth < (PLAYDATE_MAX_WIDTH - 60) do
+            local currentLineTemp = currentLine .. nextWord .. " "
+            local w, h = gfx.getTextSize(currentLineTemp)
+            if w < PLAYDATE_MAX_WIDTH - 60 then
+                currentLine = currentLineTemp
+                currentLineWidth = w
+            else
+                table.insert(words, 1, nextWord)
+                print("calc: adding " .. fontHeight)
+                totalHeight += fontHeight
+                break
+            end
+            nextWord = table.remove(words, 1)
+            if nextWord == nil then
+                break
+            end
+        end
+    end
+    print("calc: returning " .. totalHeight)
+    return totalHeight
+end
+
+function nextLineHeight()
+    local nextLine = g_preparedTextLineTable[1]
+    if nextLine then
+        return calcTotalHeight(nextLine)
+    else
+        return nil
+    end
+end
 
 function makeScreenLines()
     local m_lineCount = 0
@@ -92,6 +157,11 @@ function scroll(amount)
     local newLineCurrent = t_lineCurrent + (amount * 0.3)
     if newLineCurrent > LINE_MIN then
         t_lineCurrent = newLineCurrent
+    end
+    t_lineMax = math.max(t_lineCurrent, t_lineMax)
+    if t_lineMax - t_lineThreshold > 0 then
+        t_lineThreshold = t_lineMax + nextLineHeight()
+        addNextPreparedLine()
     end
 end
 
